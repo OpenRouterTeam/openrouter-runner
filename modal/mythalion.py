86f849beb5c0a57a83515ee6a8aca0707c2d365f
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-09-06 16:35:26 -0400

    feat: Add Mythalion to modal

--- modal/mythalion.py
+++ modal/mythalion.py
@@ -1,29 +1,46 @@
 import os
 
+from typing import List, Optional, Union
+
+from fastapi import Depends, HTTPException, status, Request
+
 from modal import Image, Secret, Stub, method, gpu, web_endpoint
-from vllm.sampling_params import SamplingParams
 from pydantic import BaseModel
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 
 MODEL_DIR = "/model"
 
 MODEL = "PygmalionAI/mythalion-13b"
 
 
+# https://github.com/vllm-project/vllm/blob/320a622ec4d098f2da5d097930f4031517e7327b/vllm/sampling_params.py#L7-L52
+
+
+class Params(BaseModel):
+    n: int = (1,)
+    best_of: Optional[int] = (None,)
+    presence_penalty: float = (0.0,)
+    frequency_penalty: float = (0.0,)
+    temperature: float = (1.0,)
+    top_p: float = (1.0,)
+    top_k: int = (-1,)
+    use_beam_search: bool = (False,)
+    length_penalty: float = (1.0,)
+    early_stopping: Union[bool, str] = (False,)
+    stop: Union[None, str, List[str]] = (None,)
+    ignore_eos: bool = (False,)
+    max_tokens: int = (16,)
+    logprobs: Optional[int] = (None,)
+
+
 class Payload(BaseModel):
     id: str
     prompt: str
-    params: SamplingParams
+    params: Params
 
 
 auth_scheme = HTTPBearer()
-
-
-async def get_token_header(access_token: str = Depends(api_key_header)):
-    if access_token != os.environ["MYTHALION_API_KEY"]:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API Key"
-        )
 
 
 def download_model_to_folder():
@@ -118,8 +135,6 @@ def completion(
     request: Request,
     token: HTTPAuthorizationCredentials = Depends(auth_scheme),
 ):
-    import os
-
     if token.credentials != os.environ["AUTH_TOKEN"]:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
