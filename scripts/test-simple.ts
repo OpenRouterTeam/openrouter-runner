a5bcf57a7fb8db539c21824f86e456e8527bf303
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-11-15 01:28:49 -0400

    Add stream test

--- scripts/test-simple.ts
+++ scripts/test-simple.ts
@@ -3,7 +3,14 @@ import { completion, runIfCalledAsScript } from 'scripts/shared';
 async function main() {
   const prompt = `USER: What was Project A119 and what were its objectives?\n\n ASSISTANT:`;
 
-  await completion(prompt);
+  await completion(
+    prompt,
+    {
+      max_tokens: 1024,
+      stop: ['</s>']
+    },
+    true
+  );
 }
 
 runIfCalledAsScript(main, import.meta.url);
