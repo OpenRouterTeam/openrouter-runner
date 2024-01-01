6d1b74b0115977d264d1b73a8ae0bdfabe40245c
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2024-01-01 11:59:58 +0700

    feat: batch-size test

---
+++ scripts/test-batch-size.ts
@@ -0,0 +1,29 @@
+import { getWordsFromFile } from 'scripts/get-words';
+import { completion, runIfCalledAsScript } from 'scripts/shared';
+
+const batchSize = 8;
+const context = 16_000;
+const inputSize = 12_000;
+const wordsCount = Math.ceil((inputSize * 4) / 3);
+const maxTokens = Math.floor(((context - inputSize) * 95) / 100);
+
+async function main() {
+  const inputs = await Promise.all(
+    [...Array(batchSize)].map(() =>
+      getWordsFromFile({
+        wordsCount,
+        startLine: Math.floor(Math.random() * 500)
+      })
+    )
+  );
+
+  await Promise.all(
+    inputs.map((prompt) =>
+      completion(prompt, {
+        max_tokens: maxTokens
+      })
+    )
+  );
+}
+
+runIfCalledAsScript(main, import.meta.url);
