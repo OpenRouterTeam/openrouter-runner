5f233c7f605f04c9fd90f1b88d375cc4cb3db9ce
sam <mistersamuelbarnes@gmail.com>
2024-03-18 08:43:31 -0600

    fix: downsize to A100 40G

--- modal/runner/containers/vllm_unified.py
+++ modal/runner/containers/vllm_unified.py
@@ -172,7 +172,7 @@ _midnight_rose_naive = "sambarnes/Midnight-Rose-70B-v2.0.3-GPTQ-naive"
 VllmContainer_MidnightRose70BNaive = _make_container(
     name="VllmContainer_MidnightRose70BNaive",
     model_name=_midnight_rose_naive,
-    gpu=modal.gpu.H100(count=1),
+    gpu=modal.gpu.A100(count=1, memory=40),
     concurrent_inputs=4,
     max_containers=1,
     quantization="GPTQ",
@@ -183,7 +183,7 @@ _midnight_rose = "sambarnes/Midnight-Rose-70B-v2.0.3-GPTQ"
 VllmContainer_MidnightRose70B = _make_container(
     name="VllmContainer_MidnightRose70B",
     model_name=_midnight_rose,
-    gpu=modal.gpu.H100(count=1),
+    gpu=modal.gpu.A100(count=1, memory=40),
     concurrent_inputs=4,
     max_containers=1,
     quantization="GPTQ",
